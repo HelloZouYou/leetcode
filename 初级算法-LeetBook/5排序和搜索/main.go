834b856a564a7e261c6e23ac7bed0960e25d1405
zouyuou-home <hellozouyou@gmail.com>
2021-06-02 01:45:17 +0800

    edit

--- 初级算法-LeetBook/5排序和搜索/main.go
+++ 初级算法-LeetBook/5排序和搜索/main.go
@@ -7,6 +7,7 @@ import "fmt"
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	t1, t2 := 0, 0
 	nums1 = nums1[:m]
+	// 双指针移动
 	for m != 0 && t1 < len(nums1) && t1 < m+n && t2 < n {
 		if nums1[t1] >= nums2[t2] {
 			// nums1 = append(nums1[:t1], append([]int{nums2[t2]}, nums1[t1:]...)...)
@@ -21,6 +22,32 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	fmt.Println(nums1)
 }
 
+// 第一个错误的版本
+func firstBadVersion(n int) int {
+	min, max := 1, n
+	// 二分法
+	for max-min > 1 {
+		mid := (min + max) / 2
+		if isBadVersion(mid) {
+			max = mid
+		} else {
+			min = mid
+		}
+	}
+	// 最后不确定的只有min，max一定是错的
+	if isBadVersion(min) {
+		return min
+	}
+	return max
+}
+
+func isBadVersion(n int) bool {
+	if n >= 5 {
+		return true
+	}
+	return false
+}
+
 func main() {
-	merge([]int{2, 0}, 1, []int{1}, 1)
+	fmt.Println(firstBadVersion(10))
 }
